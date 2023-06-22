0523ec6599b2a9c4e2f2c25e0716a81ebec595c5
rooot <hey@rooot.gay>
2023-06-22 23:08:21 +0200

    feat: cursed initial packet receiving, updated protocol
    Signed-off-by: rooot <hey@rooot.gay>

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "global.hpp"
 #include "hooks.hpp"
 #include "net.hpp"
+#include "proto/packet.pb.h"
 
 #if __APPLE__ && TARGET_OS_MAC
 #include "thread"
@@ -17,6 +18,36 @@ using namespace geode::prelude;
             switch (event.type) {
                 case ENET_EVENT_TYPE_RECEIVE: {
                     // todo!!
+                    fmt::print("recv'd packet with length {}\n", event.packet->dataLength);
+
+                    auto* gdmp_packet = new gdmp::Packet();
+                    gdmp_packet->ParseFromArray(event.packet->data, event.packet->dataLength);
+
+                    fmt::print("parsed packet, somehow.. type: {}\n", gdmp_packet->packet_type());
+
+                    if (gdmp_packet->has_player_join()) {
+                        fmt::print("a player has suddenly appeared!!\n");
+
+                        auto player_join = gdmp_packet->player_join();
+                        fmt::print("-> player id {} joined room {}\n", player_join.p_id(), player_join.room().level_id());
+
+                        // todo: handle join
+                    } else if (gdmp_packet->has_player_move()) {
+                        fmt::print("*player moving noises*\n");
+
+                        auto player_move = gdmp_packet->player_move();
+                        fmt::print("-> player id {} moved to P1[{} {}] P2[{} {}]\n", player_move.p_id(),
+                                   player_move.pos_p1().pos_x(),player_move.pos_p1().pos_y(),
+                                   player_move.pos_p2().pos_x(), player_move.pos_p2().pos_y());
+
+                        // todo: handle move
+                    } else if (gdmp_packet->has_player_leave()) {
+                        fmt::print(":vanish:\n");
+                        // todo: handle leave
+                    } else {
+                        fmt::print("wtf how\n");
+                    }
+
                     break;
                 }
                 case ENET_EVENT_TYPE_CONNECT: {
